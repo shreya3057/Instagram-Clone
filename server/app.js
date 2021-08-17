fd555786971f1ef0f6dab56cdb3cdb956b84e53c
Shashwat3057 <shreya3057@gmail.com>
2021-08-17 12:58:21 +0530

    clone

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const {MONGOURI} = require('./keys')
+const PORT = 5000
+
+//zN8KFLWCnwT99uW
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://shreya:zN8KFLWCnwT99uW@cluster0.lu8uz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"123456789"
+}
